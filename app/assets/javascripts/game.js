c82008e552c4e38b68f2f15e36aceb6a361edd1e
Adam Tse <mr.adam.tse@gmail.com>
2016-12-07 14:42:38 -0500

    box/card loading

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,16 +1,24 @@
 $(function() {
   var squares = $('.square');
+  var gameOn = false;
+  var numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 
   function start(){
-
+    gameOn = true;
+    var newnum = numbers;
     squares.each(function(){
-      $(this).css('background-color', 'red');
-    })
-    // for(var i = 0; i < squares.length; i++){
-    //   squares[i].style.background = 'blue';
-    // }
-  }
+      // $(this).css('background-color', 'red').slideUp( 3000 ).fadeIn( 400 );
+      $(this).removeClass('flash');
+      $(this).addClass('backsplash');
+      $(this).text(newnum[0]);
+      newnum.shift();
+    });
 
+  }
+  squares.hover(function(){
+    // var mouse = $(this).hasClass('backsplash');
+      $(this).toggleClass('flash');
+  });
 
   console.log('ready 1');
   $('.start').click(function(event){
@@ -18,12 +26,25 @@ $(function() {
     start();
     console.log(start);
   });
+
   $('.square').click(function(event){
     event.preventDefault();
-    console.log('clicked');
-    $(this).css('background-color', 'white');
+    if(gameOn){
+      console.log('clicked');
+    // $(this).css('background-color', 'white');
+    $(this).removeClass('backsplash');
+    check();
+  }
   });
 
 
+function check(){
+  if($('.backsplash').length === 0){
+    gameOn = false;
+  }
+}
+function ranNum(){
+
+}
 
 });
